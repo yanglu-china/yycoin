a1bb14f447ea8100b060dc435561441ec0a82955
smartman <smartman2014@qq.com>
2018-08-08 11:23:09 +0800

    ship:fixed #382 甘肃银行不需要过滤直邮订单

--- oa_job_inner/src/com/china/center/oa/job/manager/impl/GsShipJobManagerImpl.java
+++ oa_job_inner/src/com/china/center/oa/job/manager/impl/GsShipJobManagerImpl.java
@@ -1,13 +1,33 @@
 package com.china.center.oa.job.manager.impl;
 
+import com.china.center.jdbc.util.ConditionParse;
 import com.china.center.oa.sail.bean.BranchRelationBean;
 import com.china.center.oa.sail.bean.PackageItemBean;
+import com.china.center.oa.sail.vo.PackageVO;
 import com.china.center.tools.TimeTools;
+
+import java.util.List;
 
 /**
  * 甘肃银行邮件
  */
 public class GsShipJobManagerImpl extends ShipJobManagerImpl{
+
+    @Override
+    protected List<PackageVO> getPackageList() {
+        ConditionParse con = new ConditionParse();
+        con.addWhereStr();
+        con.addIntCondition("PackageBean.sendMailFlag", "=", 0);
+        con.addCondition("PackageBean.logTime", ">=", "2017-04-27 00:00:00");
+        //自提类的也不在发送邮件范围内
+        con.addIntCondition("PackageBean.shipping","!=", 0);
+        //#236 已发货和在途都要发邮件
+        con.addCondition(" and PackageBean.status in(2,10)");
+
+        //step1: 根据支行customerId+channel对CK单合并
+        List<PackageVO> packageList = packageDAO.queryVOsByCondition(con);
+        return packageList;
+    }
 
     @Override
     protected boolean needSendMail(String customerName, String channel) {
