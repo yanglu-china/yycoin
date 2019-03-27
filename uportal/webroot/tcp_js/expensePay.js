c1b97b1a6c7133cd1e0d6c29d0d1248f1861385d
smartman <smartman2014@qq.com>
2019-03-27 08:46:27 +0800

    update #612

--- uportal/webroot/tcp_js/expensePay.js
+++ uportal/webroot/tcp_js/expensePay.js
@@ -1,41 +1,48 @@
 function checkMoney1(){
-    //原申请借款金额
-    var refIdElement = document.getElementById("refId");
-    var refId = refIdElement.value;
-    var refMoney = parseFloat(document.getElementById("refMoney").value)/100;
+    var payTypeElement = document.getElementById("payType");
+    var payType = payTypeElement.value;
+    // console.log(payType);
+    //公司付款给员工
+    if (int(payType) == 1){
+        //原申请借款金额
+        var refIdElement = document.getElementById("refId");
+        var refId = refIdElement.value;
+        var refMoney = parseFloat(document.getElementById("refMoney").value)/100;
 
-    //#570 收款稽核金额
-    var pElements = document.getElementsByName('p_cmoneys');
+        //#570 收款稽核金额
+        var pElements = document.getElementsByName('p_cmoneys');
 
-    var pTotal = 0.0;
+        var pTotal = 0.0;
 
-    for (var i = 0; i<pElements.length; i++)
-    {
-        if (pElements[i].value != '')
+        for (var i = 0; i<pElements.length; i++)
         {
-            pTotal += parseFloat(pElements[i].value);
+            if (pElements[i].value != '')
+            {
+                pTotal += parseFloat(pElements[i].value);
+            }
+        }
+
+        //费用明细稽核金额
+        var fElements = document.getElementsByName('f_cmoneys');
+
+        var fTotal = 0.0;
+
+        for (var i = 0; i<fElements.length; i++)
+        {
+            if (fElements[i].value != '')
+            {
+                fTotal += parseFloat(fElements[i].value);
+            }
+        }
+        // console.log(refMoney);
+        // console.log(pTotal);
+        // console.log(fTotal);
+        if (refId && compareDouble(refMoney+pTotal, fTotal)!=0){
+            alert('原申请借款金额与收款明细稽核金额之和不等于费用明细稽核金额!');
+            return false;
         }
     }
 
-    //费用明细稽核金额
-    var fElements = document.getElementsByName('f_cmoneys');
-
-    var fTotal = 0.0;
-
-    for (var i = 0; i<fElements.length; i++)
-    {
-        if (fElements[i].value != '')
-        {
-            fTotal += parseFloat(fElements[i].value);
-        }
-    }
-    console.log(refMoney);
-    console.log(pTotal);
-    console.log(fTotal);
-    if (refId && compareDouble(refMoney+pTotal, fTotal)!=0){
-        alert('原申请借款金额与收款明细稽核金额之和不等于费用明细稽核金额!');
-        return false;
-    }
     // else if (isNull(refId) && pTotal>0 && compareDouble(pTotal, fTotal) != 0){
     //     alert('费用明细和收款明细稽核金额不一致!');
     //     return false;
