62578368e38cc3fd385b6b1de1f1fd8a525a55ca
glq0316 <39189058@qq.com>
2019-05-10 13:24:52 +0800

    update #610: 优化视图

--- uportal/db/view.sql
+++ uportal/db/view.sql
@@ -8,9 +8,15 @@ CREATE or REPLACE VIEW v_center_tcphandlehis
 AS
 SELECT his.*,
 CASE 
-WHEN (his.type>10 AND his.type<=20 AND NOT(exp.status is NULL)) THEN exp.status
-ELSE app.status
+WHEN (his.type<=10) THEN app.status
+WHEN (his.type>10 AND his.type<=20) THEN exp.status
+WHEN (his.type=21) THEN reb.status
+WHEN (his.type=22) THEN pre.status
+ELSE bac.status
 END AS status
 FROM t_center_tcphandlehis his
-LEFT JOIN t_center_tcpapply app on (app.id=his.refId and app.type=his.type)
-LEFT JOIN t_center_tcpexpense exp on (exp.id=his.refId and exp.type=his.type);
+LEFT JOIN T_CENTER_TRAVELAPPLY app on (app.id=his.refId and app.type=his.type)
+LEFT JOIN T_CENTER_TCPEXPENSE exp on (exp.id=his.refId and exp.type=his.type)
+LEFT JOIN T_CENTER_REBATE reb on (reb.id=his.refId and reb.type=his.type)
+LEFT JOIN T_CENTER_PREINVOICE pre on (pre.id=his.refId and pre.type=his.type)
+LEFT JOIN T_CENTER_BACKPREPAY_APPLY bac on (bac.id=his.refId and bac.type=his.type);
