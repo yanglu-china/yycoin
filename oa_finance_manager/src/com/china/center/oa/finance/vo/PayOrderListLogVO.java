34ee9076d4bb14fa1426a6270005fdde4484d6bb
zhangxian <164482901@qq.com>
2019-07-12 18:31:28 +0800

    资金集中支付

---
+++ oa_finance_manager/src/com/china/center/oa/finance/vo/PayOrderListLogVO.java
@@ -0,0 +1,202 @@
+package com.china.center.oa.finance.vo;
+
+import java.io.Serializable;
+
+public class PayOrderListLogVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	
+	private String outid;
+	
+	private String type;
+	
+	private String bankName;
+	
+	private String userName;
+	
+	private String bankNo;
+	
+	private String money;
+	
+	private String province;
+	
+	private String city;
+	
+	private String description; 
+	
+	private String outidtime;
+	
+	private String status;
+	
+	private String outbillid;
+	
+	private String operator;
+	
+	private String paytime;
+	
+	private String payaccount;
+	
+	private String paybank;
+	
+	private String bankstatus;
+	
+	private String bankpaytime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOutid() {
+		return outid;
+	}
+
+	public void setOutid(String outid) {
+		this.outid = outid;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getBankNo() {
+		return bankNo;
+	}
+
+	public void setBankNo(String bankNo) {
+		this.bankNo = bankNo;
+	}
+
+	public String getMoney() {
+		return money;
+	}
+
+	public void setMoney(String money) {
+		this.money = money;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getOutidtime() {
+		return outidtime;
+	}
+
+	public void setOutidtime(String outidtime) {
+		this.outidtime = outidtime;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getOutbillid() {
+		return outbillid;
+	}
+
+	public void setOutbillid(String outbillid) {
+		this.outbillid = outbillid;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public String getPaytime() {
+		return paytime;
+	}
+
+	public void setPaytime(String paytime) {
+		this.paytime = paytime;
+	}
+
+	public String getPayaccount() {
+		return payaccount;
+	}
+
+	public void setPayaccount(String payaccount) {
+		this.payaccount = payaccount;
+	}
+
+	public String getPaybank() {
+		return paybank;
+	}
+
+	public void setPaybank(String paybank) {
+		this.paybank = paybank;
+	}
+
+	public String getBankstatus() {
+		return bankstatus;
+	}
+
+	public void setBankstatus(String bankstatus) {
+		this.bankstatus = bankstatus;
+	}
+
+	public String getBankpaytime() {
+		return bankpaytime;
+	}
+
+	public void setBankpaytime(String bankpaytime) {
+		this.bankpaytime = bankpaytime;
+	}
+
+}
---
+++ oa_finance_manager/src/com/china/center/oa/finance/vo/PayOrderVO.java
@@ -0,0 +1,152 @@
+package com.china.center.oa.finance.vo;
+
+import java.io.Serializable;
+
+public class PayOrderVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String billNo;
+	
+	private String billType;
+	
+	private String billTypeDesc;
+	
+	private String billDate;
+	
+	private String billStatus;
+	
+	private String payeeBank;
+	
+	private String payeeBankAccName;
+	
+	private String payeeBankAcc;
+	
+	private String payeeAmount;
+	
+	private String remark;
+	
+	private String logTime;
+	
+	private String description;
+	
+	private String cityId;
+	
+	private String cityName;
+
+	public String getBillNo() {
+		return billNo;
+	}
+
+	public void setBillNo(String billNo) {
+		this.billNo = billNo;
+	}
+
+	public String getBillType() {
+		return billType;
+	}
+
+	public void setBillType(String billType) {
+		this.billType = billType;
+	}
+
+	public String getBillTypeDesc() {
+		return billTypeDesc;
+	}
+
+	public void setBillTypeDesc(String billTypeDesc) {
+		this.billTypeDesc = billTypeDesc;
+	}
+
+	public String getBillDate() {
+		return billDate;
+	}
+
+	public void setBillDate(String billDate) {
+		this.billDate = billDate;
+	}
+
+	public String getBillStatus() {
+		return billStatus;
+	}
+
+	public void setBillStatus(String billStatus) {
+		this.billStatus = billStatus;
+	}
+
+	public String getPayeeBank() {
+		return payeeBank;
+	}
+
+	public void setPayeeBank(String payeeBank) {
+		this.payeeBank = payeeBank;
+	}
+
+	public String getPayeeBankAccName() {
+		return payeeBankAccName;
+	}
+
+	public void setPayeeBankAccName(String payeeBankAccName) {
+		this.payeeBankAccName = payeeBankAccName;
+	}
+
+	public String getPayeeBankAcc() {
+		return payeeBankAcc;
+	}
+
+	public void setPayeeBankAcc(String payeeBankAcc) {
+		this.payeeBankAcc = payeeBankAcc;
+	}
+
+	public String getPayeeAmount() {
+		return payeeAmount;
+	}
+
+	public void setPayeeAmount(String payeeAmount) {
+		this.payeeAmount = payeeAmount;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getLogTime() {
+		return logTime;
+	}
+
+	public void setLogTime(String logTime) {
+		this.logTime = logTime;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(String cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	
+}
