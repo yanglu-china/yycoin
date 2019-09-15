c54609aec2b1e03461be6272f5f812138f3007a6
glq0316 <39189058@qq.com>
2019-09-15 21:26:16 +0800

    借款报销收支平衡，稽核金额为0，取实际金额

--- oa_tcp_inner/src/com/china/center/oa/tcp/manager/impl/ExpenseManagerImpl.java
+++ oa_tcp_inner/src/com/china/center/oa/tcp/manager/impl/ExpenseManagerImpl.java
@@ -1060,7 +1060,7 @@ public class ExpenseManagerImpl extends AbstractListenerManager<TcpPayListener> 
             }
             
             //借贷平衡，稽核金额为0，凭证金额为实际金额
-            if(totalCmoney==0 && (bean.getTotal() == bean.getBorrowTotal())){
+            if(totalCmoney==0 && bean.getPayType() == TcpConstanst.PAYTYPE_PAY_OK){
             	totalCmoney = bean.getTotal();
             }
             
