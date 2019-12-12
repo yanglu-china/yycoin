295be85642d217e5f5ac4a82d8466763478e1036
kingofhawks <kingofhawks@qq.com>
2019-12-12 22:14:22 +0800

    客户转移后，“预开票待我处理”要转移到新的业务员名下操作(#847)

--- oa_tax_glue/src/com/china/center/oa/tax/glue/listener/impl/CustomerListenerTaxImpl.java
+++ oa_tax_glue/src/com/china/center/oa/tax/glue/listener/impl/CustomerListenerTaxImpl.java
@@ -15,7 +15,20 @@ import com.china.center.jdbc.util.ConditionParse;
 import com.china.center.oa.client.bean.AssignApplyBean;
 import com.china.center.oa.client.bean.CustomerBean;
 import com.china.center.oa.client.listener.ClientListener;
+import com.china.center.oa.publics.bean.FlowLogBean;
+import com.china.center.oa.publics.constant.PublicConstant;
+import com.china.center.oa.publics.dao.FlowLogDAO;
 import com.china.center.oa.tax.dao.FinanceItemDAO;
+import com.china.center.oa.tcp.bean.TcpApproveBean;
+import com.china.center.oa.tcp.constanst.TcpConstanst;
+import com.china.center.oa.tcp.dao.TcpApproveDAO;
+import com.china.center.tools.ListTools;
+import com.china.center.tools.TimeTools;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 /**
@@ -28,7 +41,13 @@ import com.china.center.oa.tax.dao.FinanceItemDAO;
  */
 public class CustomerListenerTaxImpl implements ClientListener
 {
+    private final Log _logger = LogFactory.getLog(getClass());
+
     private FinanceItemDAO financeItemDAO = null;
+
+    private TcpApproveDAO tcpApproveDAO = null;
+
+    private FlowLogDAO flowLogDAO = null;
 
     /**
      * default constructor
@@ -72,10 +91,33 @@ public class CustomerListenerTaxImpl implements ClientListener
     /**
      * 通过其他实现
      */
+    @Override
+    @Transactional(rollbackFor = MYException.class)
     public void onChangeCustomerRelation(User user, AssignApplyBean apply, CustomerBean cus, String destStafferId)
         throws MYException
     {
         // NA
+        _logger.info("****onChangeCustomerRelation***");
+        // #847 状态为‘待申请人关联销售’的预开票会跟随客户转移到最新的业务员名下预开票待我处理中，原预开票申请中申请单不调整
+        List<TcpApproveBean> approveBeans = tcpApproveDAO.queryPreInvoiceType(TcpConstanst.TCP_STATUS_APPLY_RELATE,cus.getId());
+        if(!ListTools.isEmptyOrNull(approveBeans)){
+            _logger.info(approveBeans.size()+" approveBeans to be transferred ***"+approveBeans);
+            for (TcpApproveBean bean: approveBeans){
+                //更新处理人
+                this.tcpApproveDAO.updateApproverId(bean.getId(), destStafferId);
+
+                String oldStaffer = bean.getApproverId();
+                FlowLogBean log = new FlowLogBean();
+                log.setActor(user.getStafferName());
+                log.setDescription("变更预开票处理人,从:" + oldStaffer + "到:" + destStafferId);
+                log.setFullId(bean.getId());
+                log.setOprMode(PublicConstant.OPRMODE_PASS);
+                log.setLogTime(TimeTools.now());
+                log.setPreStatus(bean.getStatus());
+                log.setAfterStatus(bean.getStatus());
+                flowLogDAO.saveEntityBean(log);
+            }
+        }
     }
 
     /*
@@ -105,4 +147,11 @@ public class CustomerListenerTaxImpl implements ClientListener
         this.financeItemDAO = financeItemDAO;
     }
 
+    public void setTcpApproveDAO(TcpApproveDAO tcpApproveDAO) {
+        this.tcpApproveDAO = tcpApproveDAO;
+    }
+
+    public void setFlowLogDAO(FlowLogDAO flowLogDAO) {
+        this.flowLogDAO = flowLogDAO;
+    }
 }
--- oa_tcp_inner/src/com/china/center/oa/tcp/dao/impl/TcpApproveDAOImpl.java
+++ oa_tcp_inner/src/com/china/center/oa/tcp/dao/impl/TcpApproveDAOImpl.java
@@ -13,6 +13,7 @@ import com.china.center.jdbc.inter.impl.BaseDAO;
 import com.china.center.oa.tcp.bean.TcpApproveBean;
 import com.china.center.oa.tcp.dao.TcpApproveDAO;
 import com.china.center.oa.tcp.vo.TcpApproveVO;
+import java.util.List;
 
 
 /**
@@ -25,4 +26,21 @@ import com.china.center.oa.tcp.vo.TcpApproveVO;
  */
 public class TcpApproveDAOImpl extends BaseDAO<TcpApproveBean, TcpApproveVO> implements TcpApproveDAO
 {
+
+    @Override
+    public List<TcpApproveBean> queryPreInvoiceType(int status,String customerId) {
+        String sql = "select TcpApproveBean.* from t_center_tcpapprove TcpApproveBean left join t_center_preinvoice PreInvoiceBean " +
+                "on TcpApproveBean.applyId=PreInvoiceBean.id where TcpApproveBean.type in(22,24) and TcpApproveBean.status=? " +
+                "and PreInvoiceBean.customerId= ? order by TcpApproveBean.logTime desc;";
+
+        return this.jdbcOperation.queryForListBySql(sql, claz,status, customerId);
+    }
+
+    @Override
+    public boolean updateApproverId(String id, String approverId)
+    {
+        this.jdbcOperation.updateField("approverId", approverId, id, claz);
+
+        return true;
+    }
 }
