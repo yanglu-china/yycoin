8bc4ef15b161303393b9ccb7ee797918c5e0705c
kingofhawks <kingofhawks@qq.com>
2020-03-16 21:51:41 +0800

    update #918

--- script/check_black_out.py
+++ script/check_black_out.py
@@ -1,54 +1,47 @@
 # 导入必要模块
 import pandas as pd
 from sqlalchemy import create_engine
+import math
 
-# 初始化数据库连接，使用pymysql模块
-# MySQL的用户：root, 密码:147369, 端口：3306,数据库：mydb
-engine = create_engine('mysql+pymysql://root:lazio_2000@localhost:3306/uportal2')
+engine = create_engine('mysql+pymysql://root:xxx@localhost:3306/xxx')
 
 sql = '''
       select * from t_center_black_out where type=2;
       '''
-
-# read_sql_query的两个参数: sql语句， 数据库连接
 df = pd.read_sql_query(sql, engine)
-
 print(len(df))
 
-# duplicate_bool = df.duplicated(subset=['outId', 'productId', 'amount','invoiceMoney','invoiceNum','invoiceDate','mobile'], keep='first')
-# duplicate = df.loc[duplicate_bool == True]
-# duplicate = df[df.duplicated(subset=['outId', 'productId', 'amount','invoiceMoney','invoiceNum','invoiceDate','mobile'], keep=False)]
-# print(duplicate)
-# duplicate.to_csv("duplicate.xls")
-
-# 过滤掉正常的数据
-removed = 0
+error_list = []
 for index, row in df.iterrows():
     out_id = row['outId']
+    if 'gy' in out_id or 'NY' in out_id:
+        continue
+    money = float(row['money'])
     if out_id:
         try:
             sql = "select * from t_center_black_out_detail where outId='"+str(out_id)+"'"
             # print(sql)
             df1 = pd.read_sql_query(sql, con=engine)
             if len(df1)>0:
-                print(df1)
+                # print(df1)
+                value = 0
+                value2 = 0
                 for index2, row2 in df1.iterrows():
-                    value = row2[]
-            # sql2 = "select * from t_center_invoiceins where id='" + str(ins_id) + "'"
-            # df2 = pd.read_sql_query(sql2, con=engine)
-            # if len(df1)> 0 and len(df2) >0:
-            #     print(row_id, index, ins_id)
-            #     removed += 1
-            #     duplicate = duplicate.drop(index=index)
+                    value += int(row2['amount'])*float(row2['price'])
+                    value2 += int(row2['amount'])*float(row2['costprice'])
+                # print(money, value)
+                equal = math.isclose(money, value)
+                equal2 = math.isclose(money, value2)
+                equal3 = math.isclose(money, value/2)
+                equal4 = math.isclose(money, value2/2)
+                if equal or equal2 or equal3 or equal4:
+                    continue
+                else:
+                    print('{} money {} not equal to {}:{}'.format(out_id, money, value, value2))
+                    error_list.append(out_id)
         except:
             continue
 
-# for index, row in duplicate.iterrows():
-#     row_id = row['id']
-#     try:
-#         sql = "delete from t_center_invoiceins_import where id='"+str(row_id)+"'"
-#         pd.read_sql_query(sql, con=engine)
-#     except:
-#         continue
-# print(sql)
-# pd.read_sql_query(sql,con=engine)
+print(error_list)
+result = pd.DataFrame(error_list, columns=['out_id'])
+result.to_csv("error_black.xls", index=False)
